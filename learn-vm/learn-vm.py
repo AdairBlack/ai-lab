0b464d6da1d380b24d077491d6db81be31064ff9
AdairBlack <eternaledgar@qq.com>
2024-05-22 00:13:42 +0800

    add learn vm jupyter note

--- learn-vm/learn-vm.py
+++ learn-vm/learn-vm.py
@@ -1,3 +1,15 @@
+import scipy
 import numpy
+import matplotlib
+import pandas
+import statsmodels
+import sklearn
 
-print(numpy.__version__)
+print(f"scipy: {scipy.__version__}")
+print(f"numpy: {numpy.__version__}")
+print(f"matplotlib: {matplotlib.__version__}")
+print(f"pandas: {pandas.__version__}")
+print(f"statsmodels: {statsmodels.__version__}")
+print(f"sklearn: {sklearn.__version__}")
+
+
